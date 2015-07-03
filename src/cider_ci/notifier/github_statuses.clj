a6875df27db93324f73f64764d03c7dae6bc8bac
Thomas Schank <DrTom@schank.ch>
2015-07-03 20:45:09 +0200

    Improve display on GitHub, honor default gh token, eval branch update

--- src/cider_ci/notifier/github_statuses.clj
+++ src/cider_ci/notifier/github_statuses.clj
@@ -20,7 +20,13 @@
     ))
 
 
-;##############################################################################
+;### helper ###################################################################
+
+(defn get-default-github-token []
+  (let [token (-> (get-config) :github_authtoken)]
+    (if (-> token clojure.string/blank? not)
+      token
+      nil)))
 
 (defn amend-with-url-properties [params]
   (let [url (-> params :git_url URIBuilder.)
@@ -33,6 +39,7 @@
             :repository (-> path-segments
                             second
                             (.replaceAll ".git$" ""))})))
+
 
 ;### POST Status ##############################################################
 
@@ -61,12 +68,14 @@
 (defn build-body [params]
   {"state"  (map-state (:state params))
    "target_url"  (build-target-url params)
-   "description"  (str "The job \"" (:name params) "\" passed.")
-   "context"  (str "Cider-CI@" (:hostname (get-config)) "/" (:name params) )})
+   "description" (:state params)
+   "context"  (str "Cider-CI@" (:hostname (get-config)) " - " (:name params) )})
 
 (defn post-status [params]
   (catcher/wrap-with-suppress-and-log-warn
-    (let [token (:token params)
+    (let [token (or (:repository_github_authtoken params)
+                    (:default_github_authtoken params)
+                    (throw (IllegalStateException. "Neither :repository_github_authtoken nor :default_github_authtoken given")))
           url (-> params build-url)
           body (-> params build-body json/write-str)]
       (logging/debug [token url body])
@@ -76,23 +85,42 @@
                          :content-type :json })
       [url body])))
 
+
+;### shared ###################################################################
+
+(defn base-query []
+  (-> (hh/select :repositories.git_url
+                 [:repositories.github_authtoken :repository_github_authtoken]
+                 [:jobs.id  :job_id]
+                 :jobs.name
+                 :jobs.state
+                 [(get-default-github-token) :default_github_authtoken]
+                 [:commits.id :commit_id])
+      (hh/modifiers :distinct)
+      (hh/from :repositories)
+      (hh/merge-where [:or
+                       [:<> :repositories.github_authtoken nil]
+                       [:and
+                        [:= :repositories.use_default_github_authtoken true ]
+                        (hc/raw (-> (get-default-github-token) boolean))]])))
+
+(defn evaluate-update [get-repos-and-jobs]
+  (future (catcher/wrap-with-suppress-and-log-error
+            (->> (get-repos-and-jobs)
+                 (map amend-with-url-properties)
+                 (map post-status)
+                 doall))))
+
+
 ;### Job update ###############################################################
 
 (defn build-repos-and-jobs-query-by-job-id [job-id]
-  (-> (-> (hh/select :repositories.git_url
-                     [:repositories.github_authtoken :token]
-                     [:jobs.id  :job_id]
-                     :jobs.name
-                     :jobs.state
-                     [:commits.id :commit_id])
-          (hh/modifiers :distinct)
-          (hh/from :repositories)
-          (hh/merge-join :branches [:= :branches.repository_id :repositories.id])
-          (hh/merge-join :branches_commits [:= :branches.id :branches_commits.branch_id])
-          (hh/merge-join :commits [:= :commits.id :branches_commits.commit_id])
-          (hh/merge-join :jobs [:= :jobs.tree_id :commits.tree_id])
-          (hh/merge-where [:= :jobs.id job-id])
-          (hh/merge-where [:<> :repositories.github_authtoken nil]))
+  (-> (base-query)
+      (hh/merge-join :branches [:= :branches.repository_id :repositories.id])
+      (hh/merge-join :branches_commits [:= :branches.id :branches_commits.branch_id])
+      (hh/merge-join :commits [:= :commits.id :branches_commits.commit_id])
+      (hh/merge-join :jobs [:= :jobs.tree_id :commits.tree_id])
+      (hh/merge-where [:= :jobs.id job-id])
       hc/format))
 
 (defn get-repos-and-jobs-by-job-update [msg]
@@ -101,26 +129,44 @@
     (build-repos-and-jobs-query-by-job-id (:id msg))))
 
 (defn evaluate-job-update [msg]
-  (catcher/wrap-with-suppress-and-log-error
-    (->> (get-repos-and-jobs-by-job-update msg)
-         (map amend-with-url-properties)
-         (map post-status)
-         doall)))
+  (evaluate-update
+    (fn [] (get-repos-and-jobs-by-job-update msg))))
 
 ;(evaluate-job-update {:id "9fba4026-8968-4ad4-98ea-8f583c5955f5", :state "passed"})
-
-(defn listen-to-job-updates-and-fire-trigger-jobs []
+(defn listen-to-job-updates-and-fire-evaluate-job-update []
   (messaging/listen "job.updated" evaluate-job-update))
 
-;##############################################################################
+
+;### Branch update ############################################################
+
+(defn get-repos-and-jobs-by-branch-update [msg]
+  (let [query (-> (base-query)
+                  (hh/merge-join :branches [:= :branches.repository_id :repositories.id])
+                  (hh/merge-join :commits [:= :commits.id :branches.current_commit_id])
+                  (hh/merge-join :jobs [:= :jobs.tree_id :commits.tree_id])
+                  (hh/merge-where [:= :repositories.id (:repository_id msg)])
+                  (hh/merge-where [:= :branches.name (:name msg)])
+                  hc/format)]
+    (jdbc/query
+      (rdbms/get-ds)
+      query)))
+
+(defn evaluate-branch-update [msg]
+  (evaluate-update
+    (fn [] (get-repos-and-jobs-by-branch-update msg))))
+
+(defn listen-to-branch-updates-and-fire-evaluate-branch-update []
+  (messaging/listen "branch.updated" evaluate-branch-update))
+
+
+;### Initialize ###############################################################
 
 (defn initialize []
-  (listen-to-job-updates-and-fire-trigger-jobs)
-  )
+  (listen-to-job-updates-and-fire-evaluate-job-update)
+  (listen-to-branch-updates-and-fire-evaluate-branch-update))
+
 
 ;#### debug ###################################################################
 ;(logging-config/set-logger! :level :debug)
 ;(logging-config/set-logger! :level :info)
 ;(debug/debug-ns *ns*)
-
-
